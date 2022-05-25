41911fe98a871ac0fd185e173d209cc021d26141
常亚男 <949883887@qq.com>
2022-05-25 22:10:00 +0800

    no message

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from github import Github
 from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
-MD_HEAD = """## Gitblog
+MD_HEAD = """## yarbei_blog
 My personal blog using issues and GitHub Actions (随意转载，无需署名)
-[RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml)
 """
 
 BACKUP_DIR = "BACKUP"
@@ -264,8 +263,8 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     add_md_header("README.md", repo_name)
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
-
-    generate_rss_feed(repo, "feed.xml", me)
+#     生成rss
+#     generate_rss_feed(repo, "feed.xml", me)
     to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
 
     # save md files to backup folder
